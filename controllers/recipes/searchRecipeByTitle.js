c37424eec858993fc2af4046a1e47ae56ee69130
SerhiiNazarov <algazira89@gmail.com>
2023-04-03 00:01:52 +0300

    paginations

--- controllers/recipes/searchRecipeByTitle.js
+++ controllers/recipes/searchRecipeByTitle.js
@@ -2,8 +2,13 @@ const { Recipe } = require("../../models/recipes");
 
 const searchRecipeByTitle = async (req, res, next) => {
   const { title } = req.params;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
 
-  const result = await Recipe.find({ title });
+  const result = await Recipe.find({ title }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  });
 
   if (!result) {
     res.status(404);
