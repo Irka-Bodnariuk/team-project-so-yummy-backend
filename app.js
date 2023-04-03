d80ae03d8e1bfc4b13fdddd4b9a448d2cf3a7e17
yurakulinichenko <yurii.kulinichenko@intellectsoft.net>
2023-04-03 23:07:03 +0300

    added auth routes

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
 
-const { authRouter, recipesRouter } = require("./routes/api");
+const swaggerDocument = require('./swagger.json');
+
+const { authRouter, recipesRouter } = require('./routes/api');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/api/auth", authRouter);
-app.use("/api/recipes", recipesRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/recipes', recipesRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+    res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+    const { status = 500, message = 'Server error' } = err;
+    res.status(status).json({ message });
 });
 
 module.exports = app;
