ff0facfeeb648542d940b1ea4d59bdcacd9bb043
yurakulinichenko <yurii.kulinichenko@intellectsoft.net>
2023-04-05 23:24:58 +0300

    describe some routes

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -1,33 +1,33 @@
-const express = require("express");
+const express = require('express');
 
-const { authenticate } = require("../../middlewares");
+const { authenticate } = require('../../middlewares');
 
 const {
-  ctrllRecipes: {
-    getAll,
-    getCategoryList,
-    getRecipesMainPage,
-    getRecipeById,
-    getRecipeByCategory,
-    searchRecipeByIngredient,
-    searchRecipeByTitle,
-  },
-} = require("../../controllers");
+    ctrllRecipes: {
+        getAll,
+        getCategoryList,
+        getRecipesMainPage,
+        getRecipeById,
+        getRecipeByCategory,
+        searchRecipeByIngredient,
+        searchRecipeByTitle,
+    },
+} = require('../../controllers');
 
 const router = express.Router();
 
-router.get("/", authenticate, getAll);
+router.get('/', authenticate, getAll);
 
-router.get("/category-list", authenticate, getCategoryList);
+router.get('/category-list', authenticate, getCategoryList);
 
-router.get("/main-page", authenticate, getRecipesMainPage);
+router.get('/main-page', authenticate, getRecipesMainPage);
 
-router.get("/category/:category", authenticate, getRecipeByCategory);
+router.get('/category/:category', authenticate, getRecipeByCategory);
 
-router.get("/id/:id", authenticate, getRecipeById);
+router.get('/id/:id', authenticate, getRecipeById);
 
-router.get("/search/title/:query", authenticate, searchRecipeByTitle);
+router.get('/search/title/:query', authenticate, searchRecipeByTitle);
 
-router.get("/search/ingredient/:query", authenticate, searchRecipeByIngredient);
+router.get('/search/ingredient/:query', authenticate, searchRecipeByIngredient);
 
 module.exports = router;
