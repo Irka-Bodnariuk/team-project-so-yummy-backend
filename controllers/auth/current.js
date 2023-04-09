41a38793afd7da4f09a6752e942662b0d60928a4
Iryna Bodnariuk <104754511+Irka-Bodnariuk@users.noreply.github.com>
2023-04-10 01:28:12 +0300

    fix: current

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,9 +1,10 @@
 const current = async (req, res) => {
-  const { name, email } = req.user;
+  const { name, email, avatarURL  } = req.user;
 
   res.json({
     name,
     email,
+    avatarURL,
   });
 };
 
